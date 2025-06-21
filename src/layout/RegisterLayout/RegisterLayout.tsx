3dd6ca07e72ffe95b21c5aaf19146a270c229e2b
nghiakieran <lechinghia202@gmail.com>
2025-06-21 12:49:45 +0700

    chore: add react router dom

---
+++ src/layout/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,13 @@
+interface RegisterLayoutProps {
+  children?: React.ReactNode
+}
+const RegisterLayout = ({ children }: RegisterLayoutProps) => {
+  return (
+    <div>
+      RegisterLayout
+      {children}
+    </div>
+  )
+}
+
+export default RegisterLayout
---
+++ src/useRouteElements.tsx
@@ -0,0 +1,33 @@
+import { useRoutes } from 'react-router-dom'
+import ProductList from './pages/ProductList/ProductList'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import RegisterLayout from './layout/RegisterLayout/RegisterLayout'
+
+const useRouteElements = () => {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return routeElements
+}
+
+export default useRouteElements
