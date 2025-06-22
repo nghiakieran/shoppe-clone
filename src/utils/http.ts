e704a13fef903089739e20806bd8a8e135d22c49
nghiakieran <lechinghia202@gmail.com>
2025-06-22 17:08:30 +0700

    feat: add instance http with axios and config react query

---
+++ src/utils/http.ts
@@ -0,0 +1,16 @@
+import type { AxiosInstance } from 'axios'
+import axios from 'axios'
+
+class Http {
+  instance: AxiosInstance
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'https://api-ecom.duthanhduoc.com/',
+      timeout: 10000,
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
+}
+
+const http = new Http().instance
+export default http
